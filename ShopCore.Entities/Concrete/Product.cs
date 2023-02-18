36c96e1ce915242a948f80b60fe6d3a145c64812
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-02-18 13:11:40 +0300

    Add project files.

---
+++ ShopCore.Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCore.Entities.Concrete
+{
+    public class Product
+    {
+        //Çokaçok ilişki olmasını sağlamaktayız
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+
+
+    }
+}
---
+++ ShopCore.Entities/Concrete/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCore.Entities.Concrete
+{
+    public class ProductCategory
+    {
+        // çoka çok ilişkili product ve category tablolarının tanımlanması için  kullanıldı
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
